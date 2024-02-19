fa8d7b72d8e5d49efa8cc992b87dc3182c5b2797
Itcharles <124773954+Itcharles@users.noreply.github.com>
2024-02-19 17:17:19 +0100

    Update Weather Observation Station 11.sql

--- Weather Observation Station 11.sql
+++ Weather Observation Station 11.sql
@@ -13,5 +13,6 @@ Query the list of CITY names from STATION that either do not start with vowels o
 Solution:
 SELECT DISTINCT city
 FROM station
-WHERE city NOT LIKE '%a' AND city NOT LIKE '%e' AND city NOT LIKE '%i' AND city NOT LIKE '%o' AND city NOT LIKE '%u' AND city NOT LIKE 'a%' AND city NOT LIKE 'e%' AND city NOT LIKE 'i%' AND city NOT LIKE 'o%' AND city NOT LIKE 'u%'
+WHERE (city NOT LIKE '%a' AND city NOT LIKE '%e' AND city NOT LIKE '%i' AND city NOT LIKE '%o' AND city NOT LIKE '%u') 
+OR (city NOT LIKE 'a%' AND city NOT LIKE 'e%' AND city NOT LIKE 'i%' AND city NOT LIKE 'o%' AND city NOT LIKE 'u%')
 ORDER BY city
