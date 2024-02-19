1ee97730b8bbc1b31b1c5be429ee5bb716da64ce
Itcharles <124773954+Itcharles@users.noreply.github.com>
2024-02-19 16:50:15 +0100

    Update Weather Observation Station 7.sql

--- Weather Observation Station 7.sql
+++ Weather Observation Station 7.sql
@@ -1,5 +1,15 @@
 Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
 
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+  
 Solution:
 
 SELECT DISTINCT city
