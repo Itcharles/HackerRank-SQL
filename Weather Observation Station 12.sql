54002a4fbf4384c3875094bb0174ef4227914fc7
Itcharles <124773954+Itcharles@users.noreply.github.com>
2024-02-19 17:19:47 +0100

    Create Weather Observation Station 12.sql

---
+++ Weather Observation Station 12.sql
@@ -0,0 +1,14 @@
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+Solution:
+
