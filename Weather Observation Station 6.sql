0a8d6316fe63cf527175eb874cd0988e97d46699
Itcharles <124773954+Itcharles@users.noreply.github.com>
2024-02-19 16:16:59 +0100

    Update Weather Observation Station 6.sql

--- Weather Observation Station 6.sql
+++ Weather Observation Station 6.sql
@@ -2,6 +2,13 @@ Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from 
 
 Solution:
 
+MYSQL Solution:
+SELECT DISTINCT city
+FROM station
+WHERE city Like 'a%' OR city Like 'e%' OR city Like 'i%' OR city Like 'o%' OR city Like 'u%'
+
+  
+MS Server SQL Solution:
 SELECT DISTINCT city
 FROM station
 WHERE CITY Like '[AEIOU]%
