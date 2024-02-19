8332722b15e455585170ebf226cb3a8fb91cfa90
Itcharles <124773954+Itcharles@users.noreply.github.com>
2024-02-19 16:47:23 +0100

    Update Weather Observation Station 1.sql

--- Weather Observation Station 1.sql
+++ Weather Observation Station 1.sql
@@ -1,5 +1,15 @@
 Query a list of CITY and STATE from the STATION table.
 
+  +-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
 Solution:
 SELECT city, state
 FROM station
