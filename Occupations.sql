e0e20b141df5314c34ee95796ec150b74d19b314
Itcharles <124773954+Itcharles@users.noreply.github.com>
2024-02-19 19:07:08 +0100

    Update Occupations.sql

--- Occupations.sql
+++ Occupations.sql
@@ -5,3 +5,25 @@ Note: Print NULL when there are no more names corresponding to an occupation.
 
 Solution:
 
+WITH doctor AS (SELECT ROW_Number () OVER (ORDER BY name) AS num, name 
+FROM occupations 
+WHERE occupation = 'doctor'
+ORDER BY name),
+
+professor AS (SELECT ROW_Number () OVER (ORDER BY name) AS num, name 
+FROM occupations 
+WHERE occupation = 'professor'
+ORDER BY name),
+
+singer AS (SELECT ROW_Number () OVER (ORDER BY name) AS num, name 
+FROM occupations 
+WHERE occupation = 'singer'
+ORDER BY name),
+
+actor AS (SELECT ROW_Number () OVER (ORDER BY name) AS num, name 
+FROM occupations 
+WHERE occupation = 'actor'
+ORDER BY name)
+
+SELECT d.name, p.name, s.name, a.name
+FROM professor p LEFT JOIN doctor d ON p.num=d.num LEFT JOIN singer s ON p.num=s.num LEFT JOIN actor a ON p.num=a.num
