117283f3a8085b0d8b41ad35ea6deab56f89b2ca
Itcharles <124773954+Itcharles@users.noreply.github.com>
2024-02-19 16:49:50 +0100

    Update Weather Observation Station 6.sql

--- Weather Observation Station 6.sql
+++ Weather Observation Station 6.sql
@@ -1,5 +1,15 @@
 Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
 
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
 Solution:
 
 MYSQL Solution:
